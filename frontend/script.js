2c869e81986248f580295ac0c81e9ea85470e7f5
Eduardora21 <116120574+Eduardora21@users.noreply.github.com>
2026-03-09 16:42:07 -0600

    Add adventure map screen.

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,11 @@
-document.getElementById("startBtn").addEventListener("click", function() {
-    alert("Game starting soon!");
+document.getElementById("startBtn")?.addEventListener("click", function () {
+    window.location.href = "map.html";
+});
+
+const levels = document.querySelectorAll(".level");
+
+levels.forEach((level, index) => {
+    level.addEventListener("click", () => {
+        alert("Starting Level " + (index + 1));
+    });
 });
