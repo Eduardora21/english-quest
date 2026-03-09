6eb95c91c48808a79e0d63b1ebbaebeb65e0202c
Eduardora21 <116120574+Eduardora21@users.noreply.github.com>
2026-03-09 17:42:43 -0600

    Agregar que cambia de pregunta.

--- frontend/script.js
+++ frontend/script.js
@@ -7,13 +7,38 @@ const levels = document.querySelectorAll(".level");
 levels.forEach((level, index) => {
     level.addEventListener("click", () => {
         if(index === 0){
-        window.location.href = "level1.html";
+            window.location.href = "level1.html";
         }else{
-        alert("Level locked!");
+            alert("Level locked!");
         }
     });
 });
 
+
+/* QUESTIONS SYSTEM */
+
+const questions = [
+{
+word: "🍎",
+correct: "Apple",
+options: ["Apple", "Car", "House"]
+},
+{
+word: "🚗",
+correct: "Car",
+options: ["Banana", "Car", "Dog"]
+},
+{
+word: "🏠",
+correct: "House",
+options: ["Tree", "House", "Chair"]
+}
+];
+
+let currentQuestion = 0;
+
+
+/* SCORE SYSTEM */
 
 const answers = document.querySelectorAll(".answer");
 const result = document.getElementById("result");
@@ -21,15 +46,24 @@ const result = document.getElementById("result");
 let score = 0;
 const scoreDisplay = document.getElementById("score");
 
+
 answers.forEach((btn) => {
     btn.addEventListener("click", () => {
 
-        if (btn.textContent === "Apple") {
+        if (btn.textContent === questions[currentQuestion].correct) {
 
             score += 10;
             scoreDisplay.textContent = "Score: " + score;
 
             result.textContent = "Correct! You earned 10 points!";
+
+            currentQuestion++;
+
+            if(currentQuestion < questions.length){
+                loadQuestion();
+            }else{
+                result.textContent = "Level Complete! 🎉";
+            }
 
         } else {
 
@@ -40,3 +74,22 @@ answers.forEach((btn) => {
     });
 });
 
+
+/* LOAD QUESTION */
+
+function loadQuestion(){
+
+const question = questions[currentQuestion];
+
+document.querySelector("h2").textContent = "What is this word?";
+document.getElementById("word").textContent = question.word;
+
+const buttons = document.querySelectorAll(".answer");
+
+buttons.forEach((btn, index) => {
+    btn.textContent = question.options[index];
+});
+
+}
+
+loadQuestion();
